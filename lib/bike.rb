b1eb2457e31e8af3f5f438429904e844f7865cb6
tboeckmann <trb@bespecific.co.uk>
2013-05-29 11:48:15 +0100

    bikes can be tested

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,7 +1,22 @@
 
 class Bike
-  def initialize
-    
+
+  attr_reader :id
+
+  def initialize(id)
+    fix!
+    @id = id
   end
 
+  def broken?
+    @broken
+  end
+
+  def break!
+    @broken = true
+  end
+
+  def fix!
+    @broken = false
+  end
 end
--- test/bike_test.rb
+++ test/bike_test.rb
@@ -2,20 +2,29 @@ require 'minitest/autorun'
 require '../lib/bike'
 require 'minitest/spec'
 
-# class BikeTest < MiniTest::Test
-#   def test_
-
-#   end
-
-
 describe Bike do 
 
   before do
-    @bike = Bike.new
+    @bike = Bike.new(1)
+  end
+
+  it "should have an id" do
+    @bike.id.wont_be_nil
   end
 
   it "should not be broken when initialized" do
-    @bike.broken?.wont_be true
+    @bike.wont_be :broken?
   end
   
+  it "should be broken when broken" do
+    @bike.break!
+    @bike.must_be :broken?
+  end
+  
+  it "should not be broken when fixed" do
+    @bike.break!
+    @bike.fix!
+    @bike.wont_be :broken?
+  end
+
 end
